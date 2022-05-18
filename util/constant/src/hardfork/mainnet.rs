9ce0b00addcdff32969a9904f132581af72b8dc9
Boyu Yang <yangby@cryptape.com>
2022-05-18 17:09:21 +0800

    refactor: update light client protocol

--- util/constant/src/hardfork/mainnet.rs
+++ util/constant/src/hardfork/mainnet.rs
@@ -4,3 +4,7 @@ pub const CHAIN_SPEC_NAME: &str = "ckb";
 // TODO ckb2021 Update the epoch number for mainnet.
 /// First epoch number for CKB v2021
 pub const CKB2021_START_EPOCH: u64 = u64::MAX;
+
+// TODO(light-client) update the block number.
+/// First block which saves the MMR root hash into its header.
+pub const MMR_ACTIVATED_BLOCK: u64 = u64::MAX;
