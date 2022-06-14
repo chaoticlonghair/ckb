d3c79131e9525423979fbea36c89a8cb3b1f12a7
Boyu Yang <yangby@cryptape.com>
2022-06-14 17:18:20 +0800

    tmp: do not pull request

--- util/constant/src/hardfork/testnet.rs
+++ util/constant/src/hardfork/testnet.rs
@@ -2,8 +2,8 @@
 pub const CHAIN_SPEC_NAME: &str = "ckb_testnet";
 
 /// First epoch number for CKB v2021, at about 2021/10/24 3:15 UTC.
-pub const CKB2021_START_EPOCH: u64 = 3113;
+pub const CKB2021_START_EPOCH: u64 = 0;
 
 // TODO(light-client) update the block number.
 /// First block which saves the MMR root hash into its header.
-pub const MMR_ACTIVATED_BLOCK: u64 = u64::MAX;
+pub const MMR_ACTIVATED_BLOCK: u64 = 20;
